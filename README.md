9a5a60f7874ec3e3f407148783b916bd6885691c
Mark Gun <mgun.sd@gmail.com>
2021-03-04 21:22:42 +0000

    components added

--- README.md
+++ README.md
@@ -2,4 +2,21 @@
 
 react components examples
 
-[Live](https://itpbootcampfe.github.io/react-components)
+[Live](https://itpbootcampfe.github.io/react-components)\
+
+### gh-pages
+
+- `yarn add -D gh-pages`
+- then add to pakage.json :
+  {
+  ...
+  script:{
+  ...
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d build",
+  },
+  ...
+  "homepage": "https://yourwebadress.com"
+  }
+- to deploy to github run `yarn deploy`
+- go to github repo settings and select gh-page branch fror your github pages repo.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 // 1 - import the react & react DOM Libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApproveCard from './component/ApproveCard'
+import faker from 'faker'
+import CommentDetail from './component/CommentDetail'
 
 // 2 - component create JSX
 
 const App = () => {
   return (
     <div style={{ backgroundColor: 'lightblue', display:'grid', minHeight: '100vh', placeContent:'center' }}>
-      <h1 > Hello React </h1>
+       <div className="ui container comments">
+        <ApproveCard>
+          <CommentDetail
+            name="Mark" 
+            date="07/05/2020" 
+            comment="What a beautiful piece" 
+            avatar={faker.image.image()}/>
+        </ApproveCard>
+
+        <ApproveCard >
+          <div><h4>Are you happy?</h4></div>
+        </ApproveCard>
+
+        <ApproveCard>
+          <CommentDetail name="Bob" date="07/06/2019" comment="What a nice piece"avatar={faker.image.image()}/>
+        </ApproveCard>
+      </div>
     </div>
   )
 }
